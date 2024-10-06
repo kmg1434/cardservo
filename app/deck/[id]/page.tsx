f19c116d1e0ee22ab5f1a673802e273934267da3
Kevin Grady <kmg1434@gmail.com>
2024-10-06 18:53:13 -0400

    initial commit

---
+++ app/deck/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Deck_Page = ({ params }: {params:{id:number}}) => {
+  return (
+    <div>
+        <h1>Deck_Page</h1>
+        <div>Details of Deck {params.id}</div>
+    </div>
+  )
+}
+
+export default Deck_Page
---
+++ app/deck/page.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const deck = () => {
+    return (
+        <div>
+            <h1>Title of Decks</h1>
+            <Link href="">Deck_1</Link>
+            <br></br>
+            <Link href="">Deck_2</Link>
+        </div>
+    )
+}
+
+export default deck
