d271e236dc6bc1ebe9313bf48dd7ef8dbd2cc5bd
Kevin Grady <kmg1434@gmail.com>
2024-10-07 01:05:50 -0400

    add card page and dynamic route

---
+++ app/card/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card_Details = ({ params }: {params:{id:number}}) => {
+  return (
+    <div>
+        Card ID: {params.id}
+    </div>
+  )
+}
+
+export default Card_Details
---
+++ app/card/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const card = ({ params }: {params:{id:number}}) => {
+  return (
+    <div>card {params.id}</div>
+  )
+}
+
+export default card
